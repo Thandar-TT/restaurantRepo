448b0ab797f4ba547243d8b0a6a222c64cc1a829
HeynSekk <49932987+HeynSekk@users.noreply.github.com>
2019-07-07 17:34:51 +0630

    first_copying
    copy file to master branch first time

---
+++ checkOut.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Menu</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i|Roboto:400,500" rel="stylesheet">
+	<!--
+			CSS
+			============================================= -->
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/availability-calendar.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+	<!--================ Start Header Area =================-->
+	<header class="header-area">
+		<div class="container">
+			<div class="header-wrap">
+				<div class="header-top d-flex justify-content-between align-items-center navbar-expand-md">
+					<div class="col menu-left">
+						<a href="index.php">Home</a>
+						<a href="reserve.php">Reservation</a>
+						<a  href="menu.html">Place Order</a>
+					</div>
+					<div class="col-3 logo">
+						<a href="index.php"><img class="mx-auto" src="img/logo.png" alt=""></a>
+					</div>
+					<nav class="col navbar navbar-expand-md justify-content-end">
+
+						<!-- Toggler/collapsibe Button -->
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+							<span class="lnr lnr-menu"></span>
+						</button>
+
+						<!-- Navbar links -->
+						<div class="collapse navbar-collapse menu-right" id="collapsibleNavbar">
+							<ul class="navbar-nav justify-content-center w-100">
+								<li class="nav-item hide-lg">
+									<a class="nav-link" href="index.php">Home</a>
+								</li>
+								<li class="nav-item hide-lg">
+									<a class="nav-link" href="reserve.php">Reservation</a>
+								</li>
+								<li class="nav-item hide-lg">
+									<a class="nav-link" href="menu.html">Place order</a>
+								</li>
+								<!-- Dropdown -->
+								<li class="nav-item dropdown">
+									<a class="nav-link" href="welcome.php">For Staff</a>
+								</li>
+								<li class="nav-item dropdown">
+									<a class="nav-link" href="about.html">About Us</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!--================ End Header Area =================-->
+
+	<!--================ Start banner Area =================-->
+	<!--================ End banner Area =================-->
+
+	<!--================ Menu Area =================-->
+	<section class="menu-area" id="menu_area">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-5 col-md-5">
+					<div class="section-title relative">
+						<h1>
+							Request a<br>
+							delivery<br>
+						</h1>
+						<form class="booking-form" id="myForm" action="">
+						<div class="row">
+							<div class="col-lg-12 d-flex flex-column mb-20">
+								<input name="Report-name" placeholder="Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Address'"
+								 class="form-control" required="" type="text">
+							</div>
+							<div class="col-lg-12 d-flex flex-column mb-20">
+								<input name="Report-name" placeholder="Any notes?" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Any notes?'"
+								 class="form-control" required="" type="text">
+							</div>
+
+							<div class="col-lg-12 d-flex justify-content-end">
+								<button class="primary-btn dark mt-30 text-uppercase">Send a request</button>
+							</div>
+							<div class="alert-msg"></div>
+						</div>
+					</form>
+						
+					</div>
+				</div>
+				<div class="col-lg-7 col-md-7">
+					<div class="menu-list">
+						<div class="single-menu">
+							<h3>Order Detail</h3>
+							<ul class="list">
+								<li>
+									<p class="menu-item">Pasta ........... <span>$25.00</span></p>
+								</li>
+								<li>
+									<p class="menu-item">Noodle ........... <span>$25.00</span></p>
+								</li>
+							</ul>
+						</div>
+
+						<div class="single-menu">
+							<h3>Total price</h3>
+							<ul class="list">
+								<li>
+									<p class="menu-item">Total ........... <span>$50.00</span></p>
+								</li>
+							</ul>
+						</div>
+
+						
+			</div>
+		</div>
+	</section>
+	<!--================End Menu Area =================-->
+
+	<!--================ Chefs Quotes Area =================-->
+	
+	<!--================ End Call To Action Area =================-->
+
+	<!--================ Start Footer Area =================-->
+	<footer class="footer-area section-gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Top Products</h4>
+						<ul>
+							<li><a href="#">Managed Website</a></li>
+							<li><a href="#">Manage Reputation</a></li>
+							<li><a href="#">Power Tools</a></li>
+							<li><a href="#">Marketing Service</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Quick links</h4>
+						<ul>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Brand Assets</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Terms of Service</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Features</h4>
+						<ul>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Brand Assets</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Terms of Service</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Resources</h4>
+						<ul>
+							<li><a href="#">Guides</a></li>
+							<li><a href="#">Research</a></li>
+							<li><a href="#">Experts</a></li>
+							<li><a href="#">Agencies</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-4  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Newsletter</h4>
+						<p>Stay update with our latest</p>
+						<div class="" id="mc_embed_signup">
+							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get">
+								<div class="input-group">
+									<input type="text" class="form-control" name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''"
+									 onblur="this.placeholder = 'Enter Email Address '" required="" type="email">
+									<div class="input-group-btn">
+										<button class="btn btn-default" type="submit">
+											<span class="lnr lnr-arrow-right"></span>
+										</button>
+									</div>
+									<div class="info"></div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-bottom row align-items-center justify-content-between">
+				<p class="footer-text m-0 col-lg-6 col-md-12">Copyright © 2018 All rights reserved | This template is made with
+					<span class="lnr lnr-heart"></span> by <a href="#">Colorlib</a></p>
+				<div class="col-lg-6 col-sm-12 footer-social">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-dribbble"></i></a>
+					<a href="#"><i class="fa fa-behance"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--================ End Footer Area =================-->
+
+	<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+	 crossorigin="anonymous"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+	<script src="js/parallax.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ genRep.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Menu</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i|Roboto:400,500" rel="stylesheet">
+	<!--
+			CSS
+			============================================= -->
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/availability-calendar.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+	<!--================ Start Header Area =================-->
+	<header class="header-area">
+        <div class="container">
+            <div class="header-wrap">
+                <div class="header-top d-flex justify-content-between align-items-center navbar-expand-md">
+                    <div class="col menu-left">
+                        <a href="editMenu.php">Edit Menu</a>
+                        <a href="allRes.php">Reservations</a>
+                        <a href="allOrd.php">Orders</a>
+                    </div>
+                    <div class="col-3 logo">
+                        <a href="index.php"><img class="mx-auto" src="img/logo.png" alt=""></a>
+                    </div>
+                    <nav class="col navbar navbar-expand-md justify-content-end">
+
+                        <!-- Toggler/collapsibe Button -->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                            <span class="lnr lnr-menu"></span>
+                        </button>
+
+                        <!-- Navbar links -->
+                        <div class="collapse navbar-collapse menu-right" id="collapsibleNavbar">
+                            <ul class="navbar-nav justify-content-center w-100">
+                                <li class="nav-item hide-lg">
+                                    <a class="nav-link" href="editMenu.php">Edit Menu</a>
+                                </li>
+                                <li class="nav-item hide-lg">
+                                    <a class="nav-link" href="allRes.php">All Reservations</a>
+                                </li>
+                                <li class="nav-item hide-lg">
+                                    <a class="nav-link" href="allOrd.php">All Orders</a>
+                                </li>
+                                <!-- Dropdown -->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link" href="genRep.php">Generate Report</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="logout.php">Sign Out</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="reset.php">Reset Password</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+	<!--================ End Header Area =================-->
+
+	<!--================ Start banner Area =================-->
+	<section class="banner-area relative">
+		<div class="container">
+			<div class="row height align-items-center justify-content-center">
+				<div class="banner-content col-lg-5">
+					<h1>Generate Report</h1>
+					<hr>
+					<div class="breadcrmb">
+						<p>
+							<a href="welcome.php">Staff</a>
+							<span class="lnr lnr-arrow-right"></span>
+							<a href="#">Generate Report</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End banner Area =================-->
+
+	<!--================ Menu Area =================-->
+	<section class="menu-area" id="menu_area">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-5 col-md-5">
+					<div class="section-title relative">
+						<h1>
+							Generate <br>
+							monthly<br>
+							or yearly<br>
+							reports<br>
+						</h1>
+						<p></p>
+						
+					</div>
+				</div>
+				<div class="col-lg-7 col-md-7">
+					<div class="menu-list">
+						<div class="single-menu">
+							<h3>Report Detail</h3>
+							<section class="reservation-area">
+		<div class="container">
+			<div class="row align-items-center">
+					<form class="booking-form" id="myForm" action="">
+						<div class="row">
+							<div class="col-lg-12 d-flex flex-column mb-20">
+								<input name="Report-name" placeholder="Report Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Report Name'"
+								 class="form-control" required="" type="text">
+							</div>
+
+							<div class="input-group col-lg-6 mb-20">
+								<input id="event_date" class="form-control datepicker" placeholder="Report Date" onfocus="this.placeholder = ''"
+								 onblur="this.placeholder = 'Report Date'" class="form-control" required="" type="text">
+							</div>
+
+							<div class="col-lg-6 d-flex flex-column mb-20">
+								<input id="comid" class="form-control datepicker" placeholder="Composer ID" onfocus="this.placeholder = ''"
+								 onblur="this.placeholder = 'Composer ID'" class="form-control" required="" type="text">
+							</div>
+
+							<div class="col-lg-6 d-flex flex-column mb-20">
+								<input id="comid" class="form-control datepicker" placeholder="Report type" onfocus="this.placeholder = ''"
+								 onblur="this.placeholder = 'Report type'" class="form-control" required="" type="text">
+							</div>
+							<div class="col-lg-6 d-flex flex-column mb-20">
+								<input name="budget" placeholder="Month" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Month'"
+								 class="form-control" required="" type="text">
+							</div>
+							<div class="col-lg-12 d-flex flex-column">
+								<textarea class="form-control" name="post-message" placeholder="Type Report here" onfocus="this.placeholder = ''"
+								 onblur="this.placeholder = 'Type report here'" required=""></textarea>
+							</div>
+
+							<div class="col-lg-12 d-flex justify-content-end">
+								<button class="primary-btn dark mt-30 text-uppercase">Submit</button>
+							</div>
+							<div class="alert-msg"></div>
+						</div>
+					</form>
+				</div>
+				
+			
+		</div>
+	</section>
+							
+						</div>
+
+						
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Menu Area =================-->
+
+	<!--================ Chefs Quotes Area =================-->
+	
+	<!--================ End Call To Action Area =================-->
+
+	<!--================ Start Footer Area =================-->
+	<footer class="footer-area section-gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Top Products</h4>
+						<ul>
+							<li><a href="#">Managed Website</a></li>
+							<li><a href="#">Manage Reputation</a></li>
+							<li><a href="#">Power Tools</a></li>
+							<li><a href="#">Marketing Service</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Quick links</h4>
+						<ul>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Brand Assets</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Terms of Service</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Features</h4>
+						<ul>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Brand Assets</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Terms of Service</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Resources</h4>
+						<ul>
+							<li><a href="#">Guides</a></li>
+							<li><a href="#">Research</a></li>
+							<li><a href="#">Experts</a></li>
+							<li><a href="#">Agencies</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-4  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h4>Newsletter</h4>
+						<p>Stay update with our latest</p>
+						<div class="" id="mc_embed_signup">
+							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get">
+								<div class="input-group">
+									<input type="text" class="form-control" name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''"
+									 onblur="this.placeholder = 'Enter Email Address '" required="" type="email">
+									<div class="input-group-btn">
+										<button class="btn btn-default" type="submit">
+											<span class="lnr lnr-arrow-right"></span>
+										</button>
+									</div>
+									<div class="info"></div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-bottom row align-items-center justify-content-between">
+				<p class="footer-text m-0 col-lg-6 col-md-12">Copyright © 2018 All rights reserved | This template is made with
+					<span class="lnr lnr-heart"></span> by <a href="#">Colorlib</a></p>
+				<div class="col-lg-6 col-sm-12 footer-social">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-dribbble"></i></a>
+					<a href="#"><i class="fa fa-behance"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--================ End Footer Area =================-->
+
+	<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+	 crossorigin="anonymous"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+	<script src="js/parallax.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
